832cad65452393ee87ed2bb30e3ac95a7c8218d4
Fernando Har <hcm415@ha.org.hk>
2021-01-03 20:58:08 +0800

    First commit

---
+++ UIKitTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitTutorial
+//
+//  Created by Fernando Har on 3/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
